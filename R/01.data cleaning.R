c48dd3fb2e8d47c8939d4f4dcca07eb7cf59bd2b
chrisleitzinger <christelleleitzinger@gmail.com>
2022-01-13 13:54:17 -0500

    Update factors and cleanup

--- R/01.data cleaning.R
+++ R/01.data cleaning.R
@@ -8,10 +8,12 @@ clinical_data <- readRDS(paste0(here::here(), "/clinical_data.rds"))
 clinical_data1 <- clinical_data %>% 
   mutate(smokever = factor(smokever, levels = c("never", "ever"))) %>% 
   mutate(smokcurrent = factor(smokcurrent, levels = c("never", "former", "current"))) %>% 
-  mutate(packyrs = case_when(
-    smokcurrent == "never"               ~ 0, 
-    TRUE                                 ~ packyrs
-  )) %>% 
+  mutate(packyrs_cat = case_when(
+    packyrs <= 15                        ~ "≤ 15",
+    packyrs > 15                         ~ "> 15",
+    smokcurrent == "never"               ~ "none"
+  )) %>%
+  mutate(packyrs_cat = factor(packyrs_cat, levels = c("none", "≤ 15", "> 15"))) %>% 
   mutate(education = case_when(
     education == "high school graduate/GED or less"|
       education == "some college"                     ~ "didn't graduated",
@@ -19,10 +21,12 @@ clinical_data1 <- clinical_data %>%
       education == "graduate/professional"            ~ "graduated"
   )) %>% 
   mutate(education = factor(education, levels = c("graduated", "didn't graduated"))) %>% 
-  mutate(anyfhdur = case_when(
-    anyfhever == "no"                    ~ 0, 
-    TRUE                                 ~ anyfhdur
-  )) %>% 
+  mutate(anyfhdur_cat = case_when(
+    anyfhdur <= 100                      ~ "≤ 100",
+    anyfhdur > 100                       ~ "> 100",
+    anyfhever == "no"                    ~ "none"
+  )) %>%
+  mutate(anyfhdur_cat = factor(anyfhdur_cat, levels = c("none", "≤ 100", "> 100"))) %>% 
   mutate(BMI_recent_grp = case_when(
     BMI_recent <25                       ~ "<25 kg/m2",
     BMI_recent >=25 & BMI_recent <= 30   ~ "25-30 kg/m2",
@@ -32,11 +36,15 @@ clinical_data1 <- clinical_data %>%
     BMI_YA <25                           ~ "<25 kg/m2",
     BMI_YA >=25 & BMI_YA <= 30           ~ "25-30 kg/m2",
     BMI_YA > 30.0                        ~ ">30 kg/m2"
+  )) %>% 
+  mutate(stage = case_when(
+    stage == "Localized"                 ~ "Regional",
+    TRUE                                 ~ stage
+  )) %>% 
+  mutate(married = case_when(
+    married == "married/living as married"      ~ "married",
+    TRUE                                        ~ "others"
   ))
-  select(suid, BMI_recent, BMI_YA, "BMI_recent_grp", "BMI_YA_grp")
-
-
-"BMI_recent_grp", "BMI_YA_grp"
 
 
 
@@ -46,5 +54,4 @@ clinical_data1 <- clinical_data %>%
 
 
 
-
-write_rds(clinical_data, "/clinical_data.rds")
+write_rds(clinical_data1, "clinical_data1.rds")
